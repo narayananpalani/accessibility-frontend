3350f1aea86cc485edeaa0971c6072fbb02b74ca
Narayanan Palani <narayananp24@tests-MBP.ultrahub>
2024-10-21 17:12:07 +0100

    first draft of Angular UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProjectComponent} from './project/project.component'
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProjectComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'accessibility-frontend';
+  title = 'Digital Accessibility Verification Tool';
 }
